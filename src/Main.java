e19ddc37473b3063d71d4a1897d65e9a2623924f
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-09 19:39:17 +0300

    Условные операторы

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] mas = new int[]{3, 5, 4, 6, 3, 2, 4, 6, 7, 5, 3, 2, 4, 5};
 
-        System.out.println(Arrays.toString(mas));
-//        System.out.println(mas);
+        boolean t = true;
+        boolean f = false;
+
+        int a = 5;
+        int b = 10;
+        int c = 5;
+
+        System.out.println(5 == 5);// Равенства
+        System.out.println(5 < 6); //Меньше
+        System.out.println(6 > 5); // Больше
+        System.out.println(5 <= 5); // Меньше или равно
+        System.out.println(7 >= 5); // Больше или равно
+        System.out.println(7 != 5); // Не равно
 
 
-        System.out.print("[");
-
-        for (int i = 0; i < mas.length; i++) {
-            System.out.print(mas[i]);
-            System.out.print(", ");
-        }
-
-        System.out.print("]");
 
     }
 }
